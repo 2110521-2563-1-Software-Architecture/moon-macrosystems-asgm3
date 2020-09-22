3fc0178ea421707650a9c5ca1b8c64a39d6fb468
unknown <pin.1999.np@gmail.com>
2020-09-22 23:54:06 +0700

    add mvvc

---
+++ mvvm/mvvm/view_models/main_view_model.py
@@ -0,0 +1,29 @@
+from typing import List
+
+import rx
+from rx import operators as ops
+from rx.subject import BehaviorSubject
+
+from mvvm.models.entities.note import Note
+from mvvm.models.repositories.note_repository import NoteRepository
+
+
+class MainViewModel:
+
+    def __init__(self):
+        self.note_repository = NoteRepository()
+        # Create notes field as a behavior subject with note from the business logic as an initial value
+        # Your code here
+        self.notes = BehaviorSubject(self.note_repository.get_all_notes())
+
+    def add_note(self, note: str):
+        # Add note and emit event with new date to the subject
+        # Your code here
+        self.note_repository.add_note(note)
+        self.notes.on_next(self.note_repository.get_all_notes())
+
+    def clear_all(self):
+        # Clear all note and emit event with new data to the subject
+        # Your code here
+        self.note_repository.clear_all_notes()
+        self.notes.on_next(self.note_repository.get_all_notes())
