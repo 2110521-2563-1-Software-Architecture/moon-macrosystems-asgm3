051b96efddf824b4e0c89194bb4a3258af5472cc
unknown <ravipas.apk@gmail.com>
2020-09-23 00:43:51 +0700

    finish mvp

---
+++ mvp/mvp/contracts/main_contract.py
@@ -0,0 +1,26 @@
+from typing import List
+from mvp.views.base_view import BaseView
+from mvp.presenters.base_presenter import BasePresenter
+from mvp.models.entities.note import Note
+
+
+class MainContract:
+
+    class View(BaseView):
+
+        # Your code here
+        def update_view(self, items: List[Note]):
+            pass
+
+
+    class Presenter(BasePresenter):
+
+        # Your code here
+        def add_note(self, note: str):
+            pass
+
+        def get_all_notes(self):
+            pass
+
+        def clear_all(self):
+            pass
---
+++ mvp/mvp/presenters/main_presenter.py
@@ -0,0 +1,26 @@
+from typing import List
+from mvp.contracts.main_contract import MainContract
+from mvp.models.repositories.note_repository import NoteRepository
+from mvp.models.entities.note import Note
+
+
+class MainPresenter(MainContract.Presenter):
+
+    def __init__(self, view: MainContract.View, note_repository: NoteRepository):
+        MainContract.Presenter.__init__(self, view)
+        self.note_repository = note_repository
+
+    def get_all_notes(self):
+        return self.view.update_view(self.note_repository.get_all_notes())
+    
+
+    def add_note(self, note):
+        self.note_repository.add_note(note)
+        self.view.update_view(self.note_repository.get_all_notes())
+
+ 
+    def clear_all(self):
+        self.note_repository.clear_all_notes()
+        self.view.update_view(self.note_repository.get_all_notes())
+    
+
